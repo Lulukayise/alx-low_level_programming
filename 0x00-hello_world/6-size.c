a4afb88ed3d975ebc8a52e50a9929ebca88d010f
Lulukayise <lumkiletakane12@gmail.com>
2023-07-14 09:30:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main . A program that prints the size of various computer types
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf{"Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf{"Size of an int: %lu bytes(s)\n", (unsgned long)sizeof(b));
+
